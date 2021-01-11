7c2a086bec510b9aa14c151a32686f708573eef0
Yevhen <y.holomozyi@gmail.com>
2021-01-11 12:59:04 +0200

    added minor improvments

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,18 +1,3 @@
-/* export const AC: String = 'AC';
-export const NUMBER: String = 'NUMBER';
-export const ADD: String = 'ADD';
-export const SUBTRACT: String = 'SUBTRACT';
-export const DIVIDE: String = 'DIVIDE';
-export const MULTIPLY: String = 'MULTIPLY';
-export const PERCENT: String = 'PERCENT';
-export const PLUS_MINUS: String = 'PLUS_MINUS';
-export const DOT: String = 'DOT';
-export const EQUAL: String = 'EQUAL';
-export const MR: String = 'MR';
-export const MC: String = 'MC';
-export const M_PLUS: String = 'M_PLUS';
-export const M_MINUS: String = 'M_MINUS'; */
-
 enum Types {
   NUMBER = 'NUMBER',
   ADD = 'ADD',
